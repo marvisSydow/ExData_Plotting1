3a5c6b0283f7b78749775ef8dc5236c06256bc1c
marvisSydow <sydow.marvis@posteo.de>
2024-03-19 21:03:33 +0100

    updated plot1 code and created 2nd plot

--- plot1.R
+++ plot1.R
@@ -1,3 +1,14 @@
 # Program name: plot1.R
 # Author: Marvis S.
 # First Creation Date: 17-Mar-2023
+# Assumption: Downloaded zip file was extracted and file is in working directory
+
+data <- read.table("household_power_consumption.txt", sep=";",header=TRUE, na.strings = "?", as.is=3:9)
+data <- data[data$Date %in% c("1/2/2007","2/2/2007"),c("Global_active_power")]
+
+hist(data,
+      breaks=16,
+      col="red",main="Global Active Power",
+      xlab="Global Active Power (kilowatts)")
+
+
---
+++ plot2.R
@@ -0,0 +1,15 @@
+# Program name: plot2.R
+# Author: Marvis S.
+# First Creation Date: 19-Mar-2023
+# Assumption: Downloaded zip file was extracted and txt-file is in working directory
+
+data <- read.table("household_power_consumption.txt", sep=";",header=TRUE, na.strings = "?", as.is=3:9)
+data <- data[data$Date %in% c("1/2/2007","2/2/2007"),c("Global_active_power","Date","Time")]
+
+data$Datetimec<- paste(data$Date,data$Time) 
+data$Date <- as.Date(data$Date)
+data$Datetime <- strptime(data$Datetimec,format="%d/%m/%Y %H:%M:%S")
+
+str(data)
+plot(x=data$Datetime,y=data$Global_active_power,ylab="Global Active Power (kilowatts)",type='n')
+lines(x=data$Datetime,y=data$Global_active_power)
